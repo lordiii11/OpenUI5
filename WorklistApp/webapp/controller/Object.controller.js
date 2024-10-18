9165ca04ec4318511d04b154256da40ba5e21d7e
Maksim Samkevich <samkevich04@mail.ru>
2024-10-18 03:52:15 +0300

    Deleted comments and unused files from the project

--- WorklistApp/webapp/controller/Object.controller.js
+++ WorklistApp/webapp/controller/Object.controller.js
@@ -16,18 +16,7 @@ sap.ui.define([
 
 			formatter: formatter,
 
-			/* =========================================================== */
-			/* lifecycle methods                                           */
-			/* =========================================================== */
-
-			/**
-			 * Called when the worklist controller is instantiated.
-			 * @public
-			 */
 			onInit : function () {
-				// Model used to manipulate control states. The chosen values make sure,
-				// detail page is busy indication immediately so there is no break in
-				// between the busy indication for loading the view's meta data
 				var iOriginalBusyDelay,
 					oViewModel = new JSONModel({
 						busy : true,
@@ -36,27 +25,14 @@ sap.ui.define([
 
 				this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
 
-				// Store original busy indicator delay, so it can be restored later on
 				iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
 				this.setModel(oViewModel, "objectView");
 				this.getOwnerComponent().getModel().metadataLoaded().then(function () {
-						// Restore original busy indicator delay for the object view
 						oViewModel.setProperty("/delay", iOriginalBusyDelay);
 					}
 				);
 			},
 
-			/* =========================================================== */
-			/* event handlers                                              */
-			/* =========================================================== */
-
-
-			/**
-			 * Event handler  for navigating back.
-			 * It there is a history entry we go one step back in the browser history
-			 * If not, it will replace the current entry of the browser history with the worklist route.
-			 * @public
-			 */
 			onNavBack : function() {
 				var sPreviousHash = History.getInstance().getPreviousHash();
 
@@ -67,16 +43,6 @@ sap.ui.define([
 				}
 			},
 
-			/* =========================================================== */
-			/* internal methods                                            */
-			/* =========================================================== */
-
-			/**
-			 * Binds the view to the object path.
-			 * @function
-			 * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
-			 * @private
-			 */
 			_onObjectMatched : function (oEvent) {
 				var sObjectId =  oEvent.getParameter("arguments").objectId;
 				this.getModel().metadataLoaded().then( function() {
@@ -87,12 +53,6 @@ sap.ui.define([
 				}.bind(this));
 			},
 
-			/**
-			 * Binds the view to the object path.
-			 * @function
-			 * @param {string} sObjectPath path to the object to be bound
-			 * @private
-			 */
 			_bindView : function (sObjectPath) {
 				var oViewModel = this.getModel("objectView"),
 					oDataModel = this.getModel();
@@ -103,10 +63,6 @@ sap.ui.define([
 						change: this._onBindingChange.bind(this),
 						dataRequested: function () {
 							oDataModel.metadataLoaded().then(function () {
-								// Busy indicator on view should only be set if metadata is loaded,
-								// otherwise there may be two busy indications next to each other on the
-								// screen. This happens because route matched handler already calls '_bindView'
-								// while metadata is loaded.
 								oViewModel.setProperty("/busy", true);
 							});
 						},
@@ -122,7 +78,6 @@ sap.ui.define([
 					oViewModel = this.getModel("objectView"),
 					oElementBinding = oView.getElementBinding();
 
-				// No data for the binding
 				if (!oElementBinding.getBoundContext()) {
 					this.getRouter().getTargets().display("objectNotFound");
 					return;
