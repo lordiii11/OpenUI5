bddc1e8c6be939bf29c3be6eabfbc5c796efe099
Maksim Samkevich <samkevich04@mail.ru>
2024-10-26 02:33:24 +0300

    lab04/task08. Add horizontal Form with 2 columns - Format Created field via XML with style 'short' - Format Modified field using formatter in model folder with style 'medium'

--- WorklistApp/webapp/model/formatter.js
+++ WorklistApp/webapp/model/formatter.js
@@ -3,18 +3,19 @@ sap.ui.define([
 		"use strict";
 
 		return {
-
-			/**
-			 * Rounds the number unit value to 2 digits
-			 * @public
-			 * @param {string} sValue the number string to be rounded
-			 * @returns {string} sValue with 2 digits rounded
-			 */
 			numberUnit : function (sValue) {
 				if (!sValue) {
 					return "";
 				}
 				return parseFloat(sValue).toFixed(2);
+			},
+
+			modifiedFormatter(oDate) {
+				const oDateFormatter = sap.ui.core.format.DateFormat.getDateInstance({
+					style: "medium"
+				});
+			
+				return oDateFormatter.format(new Date(oDate));
 			}
 
 		};
